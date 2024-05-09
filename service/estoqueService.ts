a3e6bebb083423deca2b8fae7a0f4d47781e51d2
Márcia Bigão <mbigaod@gmail.com>
2024-05-09 15:00:18 -0300

    feat (criar no estoque): adiciona um item ao arquivo do estoque

--- service/estoqueService.ts
+++ service/estoqueService.ts
@@ -3,3 +3,35 @@ import writeCSV from "/home/mbigao/projetos/Treinee/atividade_s3/model/writeCSV.
 import { Data } from "/home/mbigao/projetos/Treinee/atividade_s3/model/interface_data.ts";
 import fs from 'fs';
 
+const filePath = '/home/mbigao/projetos/Treinee/atividade_s3/database/estoque.csv';
+
+class estoqueService
+{
+    async criar(data: Data)
+    {
+        if(typeof data.nome !== 'string' || isNaN(data.peso) || isNaN(data.valor) || isNaN(data.quantidade))
+        {
+            throw new Error("Esses dados não são válidos para o estoque.");
+        }
+        else
+        {
+            const arquivoCompleto = await readCSV(filePath);
+            const valoresNomes: string[] = [];
+
+            for(const linha of arquivoCompleto)
+            {
+                const valor = linha.nome;
+                valoresNomes.push(valor);
+            }
+
+            if(valoresNomes.includes(data.nome))
+            {
+                throw new Error("Esse item já existe no estoque.");
+            }
+            else
+            {
+                await writeCSV(filePath, [data]);
+            }
+        }
+    }
+}
