e260c0bd400d6886e5172a17531768f9a1e7a892
Márcia Bigão <mbigaod@gmail.com>
2024-05-11 17:26:42 -0300

    fix (index) : fiz funcionar a programação assíncrona do código

--- index.ts
+++ index.ts
@@ -12,16 +12,16 @@ import { totalItens } from "./controller/total_itens";
 import { totalProdutos } from "./controller/total_produtos";
 
 var prompt: string = "Digite a ação desejada:";
-prompt += "Digite 1 para adicionar item ao estoque";
-prompt += "Digite 2 para remover item ao estoque";
-prompt += "Digite 3 para listar todos os itens do estoque";
-prompt += "Digite 4 para calcular o valor total do estoque";
-prompt += "Digite 5 para calcular o peso total do estoque";
-prompt += "Digite 6 para calcular a média dos valores do estoque";
-prompt += "Digite 7 para calcular a média dos pesos do estoque";
-prompt += "Digite 8 para saber o total de itens no estoque";
-prompt += "Digite 9 para saber o total de produtos no estoque";
-prompt += "Digite 0 para voltar ao item anterior";
+prompt += "\nDigite 1 para adicionar item ao estoque";
+prompt += "\nDigite 2 para remover item ao estoque";
+prompt += "\nDigite 3 para listar todos os itens do estoque";
+prompt += "\nDigite 4 para calcular o valor total do estoque";
+prompt += "\nDigite 5 para calcular o peso total do estoque";
+prompt += "\nDigite 6 para calcular a média dos valores do estoque";
+prompt += "\nDigite 7 para calcular a média dos pesos do estoque";
+prompt += "\nDigite 8 para saber o total de itens no estoque";
+prompt += "\nDigite 9 para saber o total de produtos no estoque";
+prompt += "\nDigite 0 para voltar ao item anterior\n";
 
 
 var entrada = scan(prompt)
@@ -38,64 +38,67 @@ if(entrada == 0)
 
 W = parseInt(entrada, 10);
 
-switch(W)
-{
-    case(1):
-
-        var nome = scan("Digite o nome do produto: ");
-        var valor = scan("Digite o valor do produto: ");
-        var peso = scan("Digite o peso do produto: ");
-        var quantidade = scan("Digite a quantidade do produto");
-
-        const dados = {
-            nome: nome,
-            valor: parseFloat(valor),
-            peso: parseFloat(peso),
-            quantidade: parseFloat(quantidade),
-        } as Data;
-
-        await adicionarProduto(dados);
-        break;
-
-    case(2):
-        
-        var identificador = scan("Digite o item a ser removido do estoque: ")
-        await removerProdutos(identificador);
-        break;
-
-    case(3):
-
-        await listarProdutos();
-        break;
-
-    case(4):
-
-        await calcularValorTotal();
-        break;
-
-    case(5):
-
-        await calcularPesoTotal();
-        break;
-
-    case(6):
-        
-        await calcularMediaValores();
-        break;
-
-    case(7):
-
-        await calcularMediaPesos();
-        break;
-
-    case(8):
-
-        await totalItens();
-        break;
+(async () =>  {
     
-    case(9):
+    switch(W)
+    {
+        case(1):
 
-        await totalProdutos();
-        break;   
+            var nome = scan("Digite o nome do produto: ");
+            var valor = scan("Digite o valor do produto: ");
+            var peso = scan("Digite o peso do produto: ");
+            var quantidade = scan("Digite a quantidade do produto: ");
+
+            const dados = {
+                nome: nome,
+                valor: parseFloat(valor),
+                peso: parseFloat(peso),
+                quantidade: parseFloat(quantidade),
+            } as Data;
+
+            await adicionarProduto(dados);
+            break;
+
+        case(2):
         
-}
+            var identificador = scan("Digite o item a ser removido do estoque: ")
+            await removerProdutos(identificador);
+            break;
+
+        case(3):
+
+            await listarProdutos();
+            break;
+
+        case(4):
+
+            await calcularValorTotal();
+            break;
+
+        case(5):
+
+            await calcularPesoTotal();
+            break;
+
+        case(6):
+        
+            await calcularMediaValores();
+            break;
+
+        case(7):
+
+            await calcularMediaPesos();
+            break;
+
+        case(8):
+
+            await totalItens();
+            break;
+    
+        case(9):
+
+            await totalProdutos();
+            break;   
+        
+    }
+})();
